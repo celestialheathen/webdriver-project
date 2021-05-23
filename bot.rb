7de99f3b9538ad0d77b87f5e71a097d732be5324
celestialheathen <jingchaoyu@hotmail.com>
2021-05-23 16:28:26 -0400

    Automate sign up

--- bot.rb
+++ bot.rb
@@ -7,7 +7,13 @@ driver.navigate.to "https://stoxx.netlify.app/"
 login = driver.find_element(link_text: "Login")
 login.click
 
-sign_up = driver.find_element(css: "button.ui.button")
+
+sign_up = driver.find_element(xpath: '//*[@id="login-form"]/div/button[2]')
 sign_up.click
+
+driver.find_element(name: "name").send_keys "james"
+driver.find_element(name: "password").send_keys "james"
+
+submit = driver.find_element(link_text: "Sign up").click
 
 driver.quit
